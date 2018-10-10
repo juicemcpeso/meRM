96caf517a299720eeeb2b1d45846c71258ab4bf0
juicemcpeso <rytmcnally@gmail.com>
2018-10-09 19:31:24 -0700

    Updated the file structure, added a print funtion to the tool

--- meRM.py
+++ meRM.py
@@ -19,21 +19,21 @@ print('meRM 1.0')
 ## Load a bunch of things
 
 ## Load in the list of things
-things = pd.read_csv('things.csv')
+things = pd.read_csv('main_lists/things.csv')
 
 ## Load in the list of companies
-##companies = pd.read_csv('companies.csv')
+##companies = pd.read_csv('main_lists/companies.csv')
 
 ## Load in the list of projects
-##projects = pd.read_csv('projects.csv')
+##projects = pd.read_csv('main_lists/projects.csv')
 
 ## Load in the list of people
-##people = pd.read_csv('people.csv')
+##people = pd.read_csv('main_lists/people.csv')
 
 ## Load in the thing descriptor options
-direction = pd.read_csv('direction.csv')
-how = pd.read_csv('how.csv')
-what = pd.read_csv('what.csv')
+direction = pd.read_csv('descriptors/direction.csv')
+how = pd.read_csv('descriptors/how.csv')
+what = pd.read_csv('descriptors/what.csv')
 
 ## ---
 ## Format the data
@@ -51,5 +51,9 @@ who_input = input('Who with (person id)?:        ')
 what_input = input('What type of thing:           ')
 description_input = input('Thing description:            ')
 
+print('- - -')
+print(how_input + ' ' + direction_input + ' ' + who_input + ' | ' +
+      what_input)
+print(description_input)
     
     
